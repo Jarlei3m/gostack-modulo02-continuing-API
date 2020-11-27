88b1b1a2e5ed4d3d727da22b4d799764be55b7c8
jarlei3m <jarlei_eng.mecanica@hotmail.com>
2020-11-27 08:12:30 -0300

    features: appointment-schedule-notification

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'b48c7fed72dbcd',
+    pass: '04231c232ab8e7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
