60394325b0a8b42b50fdd856e50fff7a08d3e406
jarlei3m <jarlei_eng.mecanica@hotmail.com>
2020-12-02 15:31:20 -0300

    module02: finished

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'b48c7fed72dbcd',
-    pass: '04231c232ab8e7',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
